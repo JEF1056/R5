c8f60c5e72756f98cf244878140ae4383b506a66
Jess Edmund Fan <jessefan2002@gmail.com>
2021-02-03 12:57:21 -0800

    ok

--- src/createtask.py
+++ src/createtask.py
@@ -2,34 +2,45 @@ import io
 import os
 import json
 import numpy as np
+import tensorflow as tf
 import sentencepiece as spm
+import tensorflow_datasets as tfds
 
 with open("config.json", "r") as f:
     nq_tsv_path, max_len, out_dir=json.load(f)
 sp = spm.SentencePieceProcessor(model_file=f"{os.path.join(out_dir,'bpe')}.model")
 
-def stream(num_devices, split, debug=False):
-    with io.open(nq_tsv_path[split], mode="r", encoding="utf-8") as f:
-        print(f"~~Initialized {split} stream~~")
-        while True:
-            inputs, mask=[],[]
-            while len(inputs) < num_devices:
-                d=f.readline()
-                if d == "": f.seek(0); d=f.readline()
-                inp, tar= d.split("\t")
-                inp, tar= sp.encode(inp), sp.encode(tar)
-                if len(inp) < max_len or len(tar) < max_len:
-                    combined=inp+[1]+tar+[2]
-                    inputs.append(np.asarray(np.pad(combined, (0, max_len-len(combined))), dtype=np.int32))
-                    mask.append(np.asarray(np.pad(np.ones(len(tar)+1), (len(inp)+1, max_len-len(combined))), dtype=np.int32))
-            if debug:
-                print(f"{len(inputs[0])}\n{len(mask[0])}")
-                print(f"{inputs[0]}\n{mask[0]}")
-                ovloc=sorted(np.where(mask[0]==1))
-                print(ovloc)
-                print(sp.decode(inputs[0].tolist()[ovloc[0][0]-10:ovloc[0][0]+10]))
-                overlay = [int(inputs[0][i]) for i in ovloc[0]]
-                print(f"Overlay: {overlay}\nDecoded: {sp.decode(overlay)}")
-            inputs = np.stack(inputs)
-            mask = np.stack(mask)
-            yield inputs, inputs, mask
+def preprocess(ds):
+    def to_inputs_and_targets(ex):
+        """Map {"question": ..., "answer": ...}->{"inputs": ..., "targets": ...}."""
+        inp, tar= sp.encode(str(ex["question"])), sp.encode(str(ex["answer"]))
+        if len(inp)+len(tar)+2 > max_len: inp=inp[max_len-len(inp)-len(tar)-2:]
+        combined=inp+[1]+tar+[2]
+        inp=np.asarray(np.pad(combined, (0, max_len-len(combined))), dtype=np.int32)
+        mask=np.asarray(np.pad(np.ones(len(tar)+1), (len(inp)+1, max_len-len(combined))), dtype=np.int32)
+        return inp,inp,mask
+    return ds.map(to_inputs_and_targets, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+def nq_dataset_fn(split, shuffle_files=False):
+    # We only have one file for each split.
+    del shuffle_files
+    # Load lines from the text file as examples.
+    ds = tf.data.TextLineDataset(nq_tsv_path[split])
+    # Split each "<question>\t<answer>" example into (question, answer) tuple.
+    ds = ds.map(
+    functools.partial(tf.io.decode_csv, record_defaults=["", ""],
+                      field_delim="\t", use_quote_delim=False),
+    num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds = ds.map(lambda *ex: dict(zip(["question", "answer"], ex)))
+    return ds
+
+print("A few raw validation examples...")
+for ex in tfds.as_numpy(nq_dataset_fn("validation").take(5)):
+    print(f"{len(ex[0])}\n{len(ex[2])}")
+    print(ex)
+    ovloc=sorted(np.where(ex[2][0]==1))
+    print(ovloc)
+    st=ovloc[0][0]-10 if ovloc[0][0]-10 <=0 else 0
+    print(sp.decode(ex[0].tolist()[st:ovloc[0][0]+10]))
+    overlay = [int(ex[0][i]) for i in ovloc[0]]
+    print(f"Overlay: {overlay}\nDecoded: {sp.decode(overlay)}")
--- train.py
+++ train.py
@@ -41,14 +41,16 @@ if not os.path.exists(f"{os.path.join(args.dir,'bpe')}.model"):
 
 with open("config.json", "w") as f:
     json.dump([{"train":args.train, "validation": args.val}, args.max_length, args.dir], f)
-from src.createtask import stream
+from src.createtask import nq_dataset_fn, preprocess
 test=next(stream(trax.fastmath.device_count(), "train", debug=True))[0]
 print("(device count, tokens per device) = ", test.shape)
 del test
 
+train_ds, eval_ds=trax.data.tf_inputs.data_streams("nq_dataset_fn", data_dir=None, preprocess_fn=preprocess, shuffle_buffer_size=1024)
+
 # Training task.
 train_task = training.TrainTask(
-    labeled_data=stream(trax.fastmath.device_count(), "train"),
+    labeled_data=train_ds,
     loss_layer=tl.WeightedCategoryCrossEntropy(),
     lr_schedule=trax.lr.multifactor(),
     optimizer=trax.optimizers.Adam(),
@@ -57,7 +59,7 @@ train_task = training.TrainTask(
 
 # Evaluaton task.
 eval_task = training.EvalTask(
-    labeled_data=stream(trax.fastmath.device_count(), "validation"),
+    labeled_data=eval_ds,
     metrics=[tl.WeightedCategoryCrossEntropy(), tl.WeightedCategoryAccuracy()],
     n_eval_batches=40  # For less variance in eval numbers.
 )
