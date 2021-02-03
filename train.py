ac1c5d6033c8e38f7d83a2a1a08e7755b8491897
Jess Edmund Fan <jessefan2002@gmail.com>
2021-02-03 15:23:10 -0800

    Update train.py

--- train.py
+++ train.py
@@ -42,8 +42,9 @@ if not os.path.exists(f"{os.path.join(args.dir,'bpe')}.model"):
 with open("config.json", "w") as f:
     json.dump([{"train":args.train, "validation": args.val}, args.max_length, args.dir], f)
 from src.createtask import stream
-test=next(stream(trax.fastmath.device_count(), "train", debug=True))[0]
-print(f"(device count, tokens per device) = {test.shape}\n")
+for _ in range(5):
+    test=next(stream(trax.fastmath.device_count(), "train", debug=True))[0]
+    print(f"(device count, tokens per device) = {test.shape}\n")
 del test
 
 # Training task.
