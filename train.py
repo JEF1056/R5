4cfc53662d8b3572c2bb68fede4dc57b68a0349d
Jess Edmund Fan <jessefan2002@gmail.com>
2021-02-03 13:44:58 -0800

    yep

--- train.py
+++ train.py
@@ -43,7 +43,8 @@ with open("config.json", "w") as f:
     json.dump([{"train":args.train, "validation": args.val}, args.max_length, args.dir], f)
 from src.createtask import nq_dataset_fn, preprocess
 
-train_ds, eval_ds=trax.data.tf_inputs.data_streams(nq_dataset_fn, data_dir=None, preprocess_fn=preprocess, shuffle_buffer_size=1024)
+train_ds=nq_dataset_fn("train").shuffle(buffer_size=1024)
+eval_ds=nq_dataset_fn("validation").shuffle(buffer_size=1024)
 
 # Training task.
 train_task = training.TrainTask(
