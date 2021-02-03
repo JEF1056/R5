e4960cedde22212e7e2b5fc06510b0e082f2bec8
Jess Edmund Fan <jessefan2002@gmail.com>
2021-02-03 14:02:11 -0800

    Update createtask.py

--- src/createtask.py
+++ src/createtask.py
@@ -37,10 +37,10 @@ def nq_dataset_fn(split, shuffle_files=False):
 
 print("A few raw validation examples...")
 for ex in tfds.as_numpy(nq_dataset_fn("validation").take(5)):
-    print(ex)
     ex[0],ex[1],ex[2]=ex[0].numpy(),ex[1].numpy(),ex[2].numpy()
+    print(ex)
     print(f"{len(ex[0])}\n{len(ex[2])}")
-    ovloc=sorted(np.where(ex[2]==1).tolist())
+    ovloc=sorted(np.where(ex[2].tolist()==1))
     print(ovloc)
     st=ovloc[0][0]-10 if ovloc[0][0]-10 <=0 else 0
     print(sp.decode(ex[0].tolist()[st:ovloc[0][0]+10]))
